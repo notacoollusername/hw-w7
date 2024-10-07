fccdb50cee7bb7f7b8d046f2a80705e3c47fa051
sahar <saharhasanalizadeh@gamil.com>
2024-10-07 23:01:14 +0330

    soal1

---
+++ soal1.js
@@ -0,0 +1,8 @@
+larger.onclick=function(){
+    document.getElementById("text").style.fontSize="larger";
+
+}
+smaler.onclick=function(){
+    document.getElementById("text").style.fontSize="smaller";
+
+}
